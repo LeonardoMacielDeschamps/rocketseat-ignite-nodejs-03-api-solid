a49751e4bb19918a0f15978874d743a098fdbbdd
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-09-02 15:45:01 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,16 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const userProfileUseCase = makeGetUserProfileUseCase()
 
-  console.log(request.user.sub)
+  const { user } = await userProfileUseCase.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({
+    user,
+  })
 }
