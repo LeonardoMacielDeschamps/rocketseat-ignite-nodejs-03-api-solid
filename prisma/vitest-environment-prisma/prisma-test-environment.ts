aced3357df1f24dd53aacef5aaeec01190587a5e
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-09-02 18:10:28 -0300

    Controllers & Testes E2E - Criando test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import type { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+}
---
+++ src/http/controllers/register.spec.ts
@@ -0,0 +1,5 @@
+import { test } from 'vitest'
+
+test('register', () => {
+  console.log('register')
+})
